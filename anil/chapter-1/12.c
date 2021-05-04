18bc7e506d137144d87353fa4237c41e8fe06fbe
ANIL KUMAR <anil.paramhans@gmail.com>
2021-05-04 08:54:53 +0530

    Adding code for 11, 12

---
+++ anil/chapter-1/12.c
@@ -0,0 +1,26 @@
+/*Exercise 1-12. Write a program that prints its input one word per line.*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int ch;
+
+    while ((ch = getchar()) != EOF)
+    {
+        if (ch == ' ' || ch == '\t' || ch == '\n')
+        {
+            printf("\n");
+
+            while ((ch = getchar()) == ' ' || ch == '\t' || ch == '\n');
+
+            if (ch == EOF)
+                break;
+        }
+        printf("%c", ch);
+    }
+
+    printf("\n");
+
+    return 0;
+}
